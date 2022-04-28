1d4dd29bc8076aad322d475a32e1b15a45452f9e
rblomquist <rblomquist91@gmail.com>
2022-04-28 03:18:15 -0400

    Made changes based on feedback received

--- js/js.js
+++ js/js.js
@@ -4,17 +4,4 @@ const last_updated = document.querySelector("#last_update");
 
 copyright_year.textContent = current_date.getFullYear();
 
-// sets 2 digit values for month and day
-const month = String(current_date.getMonth()).padStart(2, "0");
-const day = String(current_date.getDay()).padStart(2, "0");
-
-// sets 2 digit values for hours, mintes, and seconds
-const hours = String(current_date.getHours()).padStart(2, "0");
-const minutes = String(current_date.getMinutes()).padStart(2, "0");
-const seconds = String(current_date.getSeconds()).padStart(2, "0");
-
-// combines values togetehr to give current date and time
-const date = `${month}/${day}/${current_date.getFullYear()}`;
-const time = `${hours}:${minutes}:${seconds}`
-
-last_updated.textContent = `${date} ${time}`
+last_updated.textContent = document.lastModified;

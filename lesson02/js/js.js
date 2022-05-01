ed67fa3220f2b22153796eea6064bb50e8d9006b
rblomquist <rblomquist91@gmail.com>
2022-05-01 00:40:30 -0400

    Completed week 2 assignments

---
+++ lesson02/js/js.js
@@ -0,0 +1,7 @@
+const current_date = new Date();
+const copyright_year = document.querySelector("#copyright_year");
+const last_updated = document.querySelector("#last_update");
+
+copyright_year.textContent = current_date.getFullYear();
+
+last_updated.textContent = document.lastModified;

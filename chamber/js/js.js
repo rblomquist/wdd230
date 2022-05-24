f40b90a92972ca27341123c60df63ca7d7e52dac
rblomquist <rblomquist91@gmail.com>
2022-05-24 10:50:31 -0400

    Week 5 completed

--- chamber/js/js.js
+++ chamber/js/js.js
@@ -10,3 +10,10 @@ date.textContent = currentdate
 
 const last_updated = document.querySelector("#last_update");
 last_updated.textContent = `${document.lastModified} EST`;
+
+const weekday = now.getDay();
+
+if (weekday == 1 || weekday ==2) {
+
+    document.querySelector(".meeting").textContent = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
+}
